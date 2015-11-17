17dbce7cc244a1f2e764e20ae7f182c6a36afe03
Javier Arboleda <javier.g.arboleda@gmail.com>
2015-11-17 11:49:53 -0800

    - Made updates for error cases: 	• App crashes on orientation change from portrait to landscape when on book detail 	• On orientation change on AddBook Activity, lose text in form
    - Made sure all strings marked with translatable = false

--- app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -30,7 +30,8 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
     private View rootView;
     private String ean;
     private String bookTitle;
-    private ShareActionProvider shareActionProvider;
+    private ShareActionProvider mShareActionProvider;
+    private Intent mShareIntent;
 
     public BookDetail(){
     }
@@ -71,7 +72,13 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         inflater.inflate(R.menu.book_detail, menu);
 
         MenuItem menuItem = menu.findItem(R.id.action_share);
-        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // error case: App crashes on orientation change from portrait to landscape when on book
+        //  detail. Below changes added to handle this case
+        if (mShareIntent != null) {
+            mShareActionProvider.setShareIntent(mShareIntent);
+        }
     }
 
     @Override
@@ -95,11 +102,15 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         bookTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
         ((TextView) rootView.findViewById(R.id.fullBookTitle)).setText(bookTitle);
 
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-        shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text)+bookTitle);
-        shareActionProvider.setShareIntent(shareIntent);
+        // error case: App crashes on orientation change from portrait to landscape when on book
+        //  detail. Below changes added to handle this case
+        mShareIntent = new Intent(Intent.ACTION_SEND);
+        mShareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        mShareIntent.setType("text/plain");
+        mShareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(mShareIntent);
+        }
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
